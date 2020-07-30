309ccdd136260762e473155a5bfd720d75c71a1e
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2020-07-30 22:16:06 +0530

    Initial commit

---
+++ app/src/main/java/com/mad_2020_g08/usingstrings/MainActivity.java
@@ -0,0 +1,61 @@
+package com.mad_2020_g08.usingstrings;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtMsg2 = findViewById(R.id.tvMsg2);
+        txtMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle","onCreate() invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle","onStart() invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Lifecycle", "OnRestart() invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Lifecycle", "OnResume() invoked");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle", "OnPause() invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle", "OnStop() invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle", "OnDestroy() invoked");
+    }
+}
